6e2a9cb30d31e601fc7e05290acf3a4f1e540f3a
vackyzhao <43746663+vackyzhao@users.noreply.github.com>
2023-09-30 21:22:50 +0800

    update input_pre_data_module
    update input_pre_data_module

--- New_PE/source/input_pre_data_module.v
+++ New_PE/source/input_pre_data_module.v
@@ -23,9 +23,10 @@
 module Input_pre_data_module
 (
 	input           din_clk,
-	input  [7:0] i_data_din,        // data 输入数据 
+  input  [7:0] i_data_din,        // data 输入数据 
   input    i_data_din_vld,        // data 输入数据有效信号
 	///////////////////////////////////////////////////////////////////
+  input  [7:0]    padding,
 	input          dout_clk,        //输出时钟
 	input                en,        //使能
 	input 			      rst_n,        //重置
@@ -33,7 +34,7 @@ module Input_pre_data_module
 	output            PEclk,        //PE时钟
   output reg [0:271]prallel_data  //34*8 272位并行数据
 );
-reg [15:0]i_conv_addr;
+reg [15:0]i_conv_addr=16'd0;
 
 //34分频
 clockDivider34 divider34_inst (
@@ -41,31 +42,36 @@ clockDivider34 divider34_inst (
     .divided(PEclk)
   );
   // 实例化被测试的模块
-  PingPongBuffer PingPongBuffer_inst (
-    .i_clk_input(din_clk),
-    .i_clk_output(dout_clk),
-    .i_rst_n(i_rst_n),
-    .en(en),
-    .i_switch_pingpong(i_switch_pingpong),
-    .i_data_din(i_data_din),
-    .i_data_din_vld(i_data_din_vld),
-    .i_conv_addr(i_conv_addr),
-    .o_conv_dout(o_conv_dout),
-    .o_pl_buffer_ready(o_pl_buffer_ready)
-  );
-  always@(posedge PEclk)begin//PEclk上升沿重置i_conv_addr
-		i_conv_addr=16'b0;
+    PingPongBuffer PingPongBuffer_inst (
+    .i_clk_input(din_clk),           // 输入数据时钟
+    .i_clk_output(dout_clk),         // 输出数据时钟
+    .i_rst_n(rst_n),                 // 复位信号
+    .en(en),                         // 使能信号
+    .i_switch_pingpong(i_switch_pingpong), // 缓存切换信号
+    .i_data_din(i_data_din),         // 输入数据
+    .i_data_din_vld(i_data_din_vld), // 输入数据有效信号
+    .i_conv_addr(i_conv_addr),       // 输入索引地址
+    .o_conv_dout(o_conv_dout),       // 输出转换后的数据
+    .o_pl_buffer_ready(o_pl_buffer_ready) // 输出缓冲区就绪信号
+);
+
+  always@(posedge PEclk)begin    //PEclk上升沿重置i_conv_addr
+    if(i_conv_addr==16'd1156)begin
+      i_conv_addr=16'b0;
+    end
+		
   end
 
-  always@(posedge dout_clk or negedge rst_n)//dout_clk上升沿i_conv_addr递增
+  always@(posedge dout_clk or negedge rst_n)  begin  //dout_clk上升沿i_conv_addr递增
   if(!rst_n)begin
      i_conv_addr=16'b0;
   end else begin
-   if(PEclk==1)begin
+   if(i_conv_addr<16'd1156)begin
     i_conv_addr=i_conv_addr;
-    prallel_data[8*i_conv_addr:8*i_conv_addr+7]=o_conv_dout;
+    prallel_data[8*i_conv_addr +: 7] = o_conv_dout;
     i_conv_addr=i_conv_addr+1'b1;
     end
+  end
   end
 
 
--- New_PE/testbench/tb_input_pre_data_module.v
+++ New_PE/testbench/tb_input_pre_data_module.v
@@ -1,20 +1,16 @@
 `timescale 1ns / 1ps
 
 module tb_Input_pre_data_module;
-
-  // 时钟信号
+  
   reg din_clk;
-  reg dout_clk;
-
-  // 输入数据和控制信号
   reg [7:0] i_data_din;
   reg i_data_din_vld;
+  reg dout_clk;
   reg en;
   reg rst_n;
   reg i_switch_pingpong;
-
-  // 时钟分频信号
   wire PEclk;
+  wire [0:271] prallel_data;
 
   // 实例化被测试的模块
   Input_pre_data_module dut (
@@ -25,41 +21,44 @@ module tb_Input_pre_data_module;
     .en(en),
     .rst_n(rst_n),
     .i_switch_pingpong(i_switch_pingpong),
-    .PEclk(PEclk)
+    .PEclk(PEclk),
+    .prallel_data(prallel_data)
   );
 
-  // 初始化
+  // 初始化信号
   initial begin
-    // 初始化输入信号
     din_clk = 0;
-    dout_clk = 0;
-    i_data_din = 8'b0;
+    i_data_din = 8'h00;
     i_data_din_vld = 0;
-    en = 0;
+    dout_clk = 0;
+    en = 1 ;
     rst_n = 1;
     i_switch_pingpong = 0;
 
-    // 启动仿真
-    // 可以在这里设置输入信号的值，以及观察模块的输出
-    // 例如：
-    // i_data_din = 8'b10101010;
-    // i_data_din_vld = 1;
-    // en = 1;
-    // rst_n = 0;
+    // 模拟时钟
+    //forever #5 din_clk = ~din_clk;
+    forever #5 dout_clk = ~dout_clk;
+
+    // 初始化其他测试信号
+    // 这里可以添加测试用例的代码
+
+    // 停顿一段时间，然后开始测试
+    #100;
+
+    // 设置使能信号和其他输入
+    en = 1;
+    i_data_din_vld = 1;
+    // 设置测试用例的输入数据
+
+    // 继续模拟直到完成测试
+    #1000;
+    
+    // 在测试完成后添加任何必要的检查或打印结果的代码
+
+    // 终止仿真
+    $finish;
   end
 
-parameter Tclk = 10;
- initial begin
- // define clk
-clk = 0;
- forever #(Tclk / 2) dout_clk = ~dout_clk;
- end
-
-  // 时钟生成
-  always begin
-    
-
-    
-  end
+  // 添加任何必要的时序检查或输出结果的代码
 
 endmodule
