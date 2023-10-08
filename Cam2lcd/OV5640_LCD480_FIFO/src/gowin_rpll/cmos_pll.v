0c6e5f3885447c942aab80e737817f387e7cefb2
vackyzhao <vackyzhao@iCloud.com>
2023-10-09 01:04:35 +0800

    add code about camera
    add code about camera

---
+++ Cam2lcd/OV5640_LCD480_FIFO/src/gowin_rpll/cmos_pll.v
@@ -0,0 +1,16 @@
+//Copyright (C)2014-2022 Gowin Semiconductor Corporation.
+//All rights reserved.
+//File Title: IP file
+//GOWIN Version: V1.9.8.07 Education
+//Part Number: GW2A-LV18PG256C8/I7
+//Device: GW2A-18C
+//Created Time: Thu Aug 18 22:10:45 2022
+
+module cmos_pll (clkout, clkin);
+
+output clkout;
+input clkin;
+
+assign clkout=clkin;
+
+endmodule //cmos_pll
