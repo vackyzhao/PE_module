9d462f65598ac6e40f5555208cce60d9391850d3
vackyzhao <vackyzhao@iCloud.com>
2023-11-08 23:50:12 +0800

    update
    update

--- New_PE/testbench/tb_top_ConvLayer2.v
+++ New_PE/testbench/tb_top_ConvLayer2.v
@@ -15,7 +15,8 @@ module tb_top_convlayer2;
 
   wire [95:0] Psum_d_out;
 
-  wire conv_en;
+wire [2:0]conv_counter;
+  wire dout_vald;
 
   top_convlayer2 uut (
       .clk(clk),
@@ -31,8 +32,8 @@ module tb_top_convlayer2;
       .Filtr_in_1(Filtr_in_1),
       .Filtr_in_0(Filtr_in_0),
       .Psum_d_out(Psum_d_out),
-     
-      .conv_en(conv_en)
+     .conv_counter(conv_counter),
+      .dout_vald(dout_vald)
   );
 
   // 时钟生成
@@ -57,28 +58,25 @@ module tb_top_convlayer2;
     #5 din_valid = 1;
     #10 din_valid = ~din_valid;
     #10 din_valid = ~din_valid;
-    //data_in_0 = 96'b1;
+    data_in_0 = 96'b1;
     #10 din_valid = ~din_valid;
     #10 din_valid = ~din_valid;
-    ///data_in_0 = 96'b0;
+    data_in_0 = 96'b0;
     #10 din_valid = ~din_valid;
     #10 din_valid = ~din_valid;
-    //data_in_0 = 96'b1;
+    data_in_0 = 96'b1;
     #10 din_valid = ~din_valid;
     #10 din_valid = ~din_valid;
-    //data_in_0 = 96'b0;
+    data_in_0 = 96'b0;
     #10 din_valid = ~din_valid;
     #10 din_valid = ~din_valid;
-    //data_in_0 = 96'b1;
+    data_in_0 = 96'b1;
     #10 din_valid = ~din_valid;
     #10 din_valid = ~din_valid;
-    //data_in_0 = 96'b0;
+    data_in_0 = 96'b0;
     #10 din_valid = ~din_valid;
     #10 din_valid = ~din_valid;
-    //data_in_0 = 96'b1;
-    #10 din_valid = ~din_valid;
-    #10 din_valid = ~din_valid;
-    #10 din_valid = ~din_valid;
+    data_in_0 = 96'b1;
     #10 din_valid = ~din_valid;
     #10 din_valid = ~din_valid;
     #10 din_valid = ~din_valid;
@@ -90,14 +88,17 @@ module tb_top_convlayer2;
     #10 din_valid = ~din_valid;
     #10 din_valid = ~din_valid;
     #10 din_valid = ~din_valid;
-    //data_in_0 = 96'd5;
     #10 din_valid = ~din_valid;
     #10 din_valid = ~din_valid;
-    //data_in_0 = 96'd5;
+    #10 din_valid = ~din_valid;
+    data_in_0 = 96'd5;
+    #10 din_valid = ~din_valid;
+    #10 din_valid = ~din_valid;
+    data_in_0 = 96'd5;
     #10 din_valid = ~din_valid;
     pool_end = ~pool_end;
     #10 pool_end = ~pool_end;
-    // 设置输入数据和权重数据
+    // 设置输入数据和权重数�?
     data_in_0  = 96'b1101010101101010110101010110101011010101011010101101010101101010;
     data_in_1  = 96'b0011001100110011001100110011001100110011001100110011001100110011;
     data_in_2  = 96'b1111000011110000111100001111000011110000111100001111000011110000;
@@ -105,7 +106,7 @@ module tb_top_convlayer2;
     Filtr_in_2 = 48'b110011001100110011001100110011001100110011001100;
     Filtr_in_1 = 48'b001100110011001100110011001100110011001100110011;
     Filtr_in_0 = 48'b111100001111000011110000111100001111000011110000;
-    // 等待一段时间，模拟运行
+    // 等待�?段时间，模拟运行
     #5000;
 
     #10 din_valid = ~din_valid;
@@ -143,8 +144,8 @@ module tb_top_convlayer2;
     pool_end = ~pool_end;
     #10 pool_end = ~pool_end;
     #5000;
-    // 在这里添加其他测试操作
-    // 检查模拟结束条件
+    // 在这里添加其他测试操�?
+    // �?查模拟结束条�?
     $finish;
   end
 
